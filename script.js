fb149f3660a3f40237cf63e19df8692b642a4ca2
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-09-18 11:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	let ans=str.split(" ").join();
+	let i=0;let j=ans.length-1;
+	while(i<j){
+		if(ans.charAt(i)!=ans.charAt(j)){
+			return false;
+		}
+	}
+	retur true;
 }
 module.exports = palindrome
